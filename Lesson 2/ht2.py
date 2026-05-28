0328dcba21bb21073630c98735c46cdf36f5dc0b
CODISTAR <shreekar6769@gmail.com>
2026-05-28 10:26:50 +0530

    ACP

--- Lesson 2/ht2.py
+++ Lesson 2/ht2.py
@@ -1,7 +1,11 @@
-bday1="12jan 2015"
-bday2="6jan 2015"
+bday1="12 jan 2015"
+bday2="6 jan 2015"
 bday3="24 jan 2015"
-bday4=30 jan 2015
-bday5=18 jan 2015
+bday4="30 jan 2015"
+bday5="18 jan 2015"
 
 print(bday1)
+print(bday2)
+print(bday3)
+print(bday4)
+print(bday5)
