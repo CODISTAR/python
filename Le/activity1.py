fa9771c45c989d53a9b2b3f406f24640cc3e4978
CODISTAR <shreekar6769@gmail.com>
2026-06-08 20:46:14 +0530

    lalllajxjcx

---
+++ Le/activity1.py
@@ -0,0 +1,13 @@
+a=10
+b=12
+if a>5 and b>5:
+    print("Both a and b are greater than 5")
+else:
+    print('Both a and b are not greater than 5')
+a=10
+b=12
+if a>5 or b>5:
+    print("one of them is greater than 5")
+else:
+    print("no")
+    
---
+++ Le/activity2.py
@@ -0,0 +1,13 @@
+# activity 1
+a = ("french")
+b = ("science")
+if a!=b:
+    print("a and b are not the same")
+else:
+    print("a and b are the same")
+# activity 2
+num=int(input("Enter a number:"))
+if num%2!=0:
+    print ("the number is odd")
+else:
+    print("the number is even")
