eabff00beb64b3f640f8bef2527b60ad66b02b19
CODISTAR <shreekar6769@gmail.com>
2026-06-14 12:06:59 +0530

    F

--- LESSON 7/activity3.py
+++ LESSON 7/activity3.py
@@ -5,7 +5,7 @@ c=int(input("enter marks for subject 3: "))
 d=int(input("enter marks for subject 4: "))
 e=int(input("enter marks for subject 5: "))
 tatl= a+b+c+d+e
-avg= tatl/5
+avg= int(tatl/5)
 validrange=(range(0,101))
 if avg not in validrange:
     print("invalid input. please enter marks between 0 and 100.")
@@ -20,4 +20,4 @@ elif avg in range(60, 70):
 elif avg in range(50, 60):
     print("grade E")
 else:
-    print("grade F t1ry again next time work harder")
+    print("grade F try again next time work harder")
